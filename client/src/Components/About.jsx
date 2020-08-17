d558a8a00137c561646cc095fa40b3f37162191e
Bpmauge <briahanamauge@pursuit.org>
2020-08-17 15:27:38 -0400

    add components and edit about file

---
+++ client/src/Components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return(
+        <>
+        <h1>About</h1>
+        <p>This TV Watchlist App clone was built by Briahana as part of her CTA Retake for job-readiness .</p>
+        <p>This web app was built using Express and React and serves as a hub for keeping track of shows and movies watched by users and others.</p>
+    </>
+    )
+}
+
+export default About;
