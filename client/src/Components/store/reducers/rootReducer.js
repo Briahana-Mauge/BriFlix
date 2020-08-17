c38ed02880905eb26e44be8a97887452d0798075
Bpmauge <briahanamauge@pursuit.org>
2020-08-17 16:16:32 -0400

    create all store files to maintain history of logged user

---
+++ client/src/Components/store/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+
+import loggedUserReducer from './loggedUserReducer';
+
+const rootReducer = combineReducers({
+  loggedUser: loggedUserReducer,
+})
+
+export default rootReducer;
